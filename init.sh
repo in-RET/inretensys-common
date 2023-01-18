444d399242330a4d76d6e1b85eaba281b2b41207
Andreas Lubojanski <andreas.lubojanski@hs-nordhausen.de>
2023-01-18 17:42:25 +0100

    add comments to init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
+
+echo -e "\e[32mClone Repositories ... \e[39m"
 git clone https://github.com/in-RET/in.RET-EnSys-open-plan-GUI.git "inretensys-gui"
 git clone https://github.com/in-RET/inretensys-fastapi.git
 git clone https://github.com/in-RET/inretensys-backend.git
 
+echo -e "\e[32mChange folder to backend ... \e[39m"
 cd inretensys-backend
 
 # create environment
+echo -e "\e[32mCreate virtual environment and build wheel-file ... \e[39m"
 python3 -m venv venv
 source venv/bin/activate
 
@@ -18,5 +22,6 @@ python -m build --wheel
 deactivate
 cd ..
 
+echo -e "\e[32mCopy generated wheel-file ... \e[39m"
 cp inretensys-backend/dist/InRetEnsys-0.2a4-py3-none-any.whl inretensys-fastapi/
 cp inretensys-backend/dist/InRetEnsys-0.2a4-py3-none-any.whl inretensys-gui/app/
